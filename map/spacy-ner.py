f403763a12c1835b6aa26e213615ce36ed41344a
Thomas Luechtefeld <tom@insilica.co>
2022-08-19 14:39:59 -0400

    spacy download works

--- map/spacy-ner.py
+++ map/spacy-ner.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
-import json, math, os, spacy, time, uuid, time, asyncio, re
+import json, math, os, spacy, time, uuid, time, asyncio, re, requests, pickle
+
+def get_spacy_model(url="https://s3.amazonaws.com/ins.pins/spacy-entox.pickle"):
+    file="data/spacy-entox.pickle"
+    if not os.path.exists(file):
+        with open(file, 'wb') as fout:
+            response = requests.get(url, stream=True)
+            for block in response.iter_content(4096): 
+                fout.write(block)
+    return pickle.load(open("data/spacy-entox.pickle","rb"))
 
 def web_annotation(start, end, tag, text):
     return { '@context': 'http://www.w3.org/ns/anno.jsonld',
@@ -20,7 +29,7 @@ def get_answer(nlp, event, label, reviewer):
     data['timestamp'] = math.floor(time.time())
     return {'data': data, 'type':'label-answer'}
 
-async def tcp_echo_client():
+async def main():
     ihost,iport = os.environ["SR_INPUT"].split(":")
     sr_input, _ = await asyncio.open_connection(ihost, iport)
     
@@ -28,8 +37,8 @@ async def tcp_echo_client():
     _, sr_output = await asyncio.open_connection(ohost, oport)
 
     config = json.load(open(os.environ['SR_CONFIG']))
-    annotation_label = config['current_labels'][0]
-    nlp = spacy.load(config['current_step']['model'] + '/model-last')
+    annotation_label = config['current_labels'][0] # TODO how to get the annotation label?
+    nlp = get_spacy_model(config['current_step']['model'])
 
     while True:
         line = await sr_input.readline()
@@ -41,7 +50,7 @@ async def tcp_echo_client():
         event = json.loads(line.decode().rstrip())
         if event['type'] == 'document' and event['data'].get('abstract'):
             answer = json.dumps(get_answer(nlp, event, annotation_label, config['reviewer']))
-            sr_output.write(f"{answer}\n".encode())
+            sr_output.write(f"{answer}\n".encode()) # TODO python library should provide all this logic
             await sr_output.drain()
 
-asyncio.run(tcp_echo_client())
+asyncio.run(main())
