5831d04df5101ca2b79fe3c798fd80a8fce9fb97
John Shaffer <jdsha@proton.me>
2022-11-07 16:30:01 -0600

    Convert babashka-bashed HTML steps to use the embedded CLI step

--- src/html_label.clj
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bb
-
-(require '[babashka.deps :as deps])
-
-(deps/add-deps '{:deps {co.insilica/bb-srvc {:mvn/version "0.8.0"}}})
-
-(require '[srvc.bb.html :as bhtml])
-
-(bhtml/serve (bhtml/resource-path *file*) "public/label.html")
